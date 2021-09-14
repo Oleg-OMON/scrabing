22ca86812ebf755e7f6eb4abb1bae52fb354087b
Oleg Zasedatelev <ol.zasedatelew@yandex.ru>
2021-09-14 15:05:28 +0300

    added file

---
+++ scaping_aria.py
@@ -0,0 +1,39 @@
+import json
+from bs4 import BeautifulSoup
+import requests
+
+
+def get_data(url):
+    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/605.1.15 (KHTML, '
+                             'like Gecko) Version/13.1 Safari/605.1.15'}
+
+    # req = requests.get(url, headers=headers)
+    #
+    # with open('aria_wiki.html', 'w') as file:
+    #     file.write(req.text)
+
+    with open('aria_wiki.html',) as file:
+        src = file.read()
+
+    soup = BeautifulSoup(src, 'lxml')
+    albumn_all = soup.find('table', class_='wikitable').find_all('th')
+    result_json = []
+    for albumn in albumn_all[3:]:
+        albumn_name = albumn.find('a').get('title')
+
+
+        soup = BeautifulSoup(src, 'lxml')
+        release_all= soup.find('table', class_='wikitable').find_all('ul')
+
+        for release in release_all:
+            release_date = release.find('li').text
+            # print(release_date)
+        result = {albumn_name: release_date}
+        result_json.append(result)
+
+    resp = open('aria_albumn.json', 'w+', encoding='utf-8')
+    json.dump(result_json, resp, ensure_ascii=False, indent=4)
+
+
+
+get_data('https://ru.wikipedia.org/wiki/Дискография_группы_«Ария»#Студийные_альбомы')
